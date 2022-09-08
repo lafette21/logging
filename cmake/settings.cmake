a9e3d13bf8785eced832b6b9b6287affc84daf63
laFette21 <viragh.akos21@gmail.com>
2022-09-08 16:10:32 +0200

    Add Conan for dependency handling
    Change-Id: Icc431ce36f7049c5ee398e0a5aa9f5788a55743f

--- cmake/settings.cmake
+++ cmake/settings.cmake
@@ -1,3 +1,16 @@
 include_guard()
 
+# Not ideal to use this global variable, but necessary to make sure
+# that tooling and projects use the same version
+set(CMAKE_CXX_STANDARD 20)
+
+# Strongly encouraged to enable this globally to avoid conflicts between
+# -Wpedantic being enabled and -std=c++20 and -std=gnu++20 for example
+# when compiling with PCH enabled
+set(CMAKE_CXX_EXTENSIONS OFF)
+
+if(NOT DEFINED CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE Release)
+endif()
+
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
--- src/logging.h
+++ src/logging.h
@@ -1,9 +1,15 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
+#include <spdlog/spdlog.h>
+
 namespace logging
 {
-
+    template<typename... T>
+    inline void info(fmt::format_string<T...> fmt, T&& ...args)
+    {
+        spdlog::info(fmt, std::forward<T>(args)...);
+    }
 }
 
 #endif // LOGGING_H
