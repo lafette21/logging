3136b348fd10a953535e9357c267b45bf2235f88
laFette21 <viragh.akos21@gmail.com>
2023-02-12 07:32:45 +0100

    Upgrade dependency version
    - spdlog 1.10.0 -> 1.11.0
    
    Change-Id: I9b8402f324595fc5399516f105e68a8054cd5f78

--- conanfile.py
+++ conanfile.py
@@ -2,14 +2,14 @@ from conans import ConanFile
 
 class Logging(ConanFile):
     name = "logging"
-    version = "0.1.2"
+    version = "0.2.0"
     description = "C++ logging wrapper library."
     url = "https://github.com/laFette21/logging"
     exports_sources = "include/*"
     no_copy_source = True
 
     def requirements(self):
-        self.requires("spdlog/1.10.0")
+        self.requires("spdlog/1.11.0")
 
     def package(self):
         self.copy("*.h")
